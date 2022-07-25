9497ec1484a4c2ca770113f27324670f22d77b89
mahbrito <104012565+mahbrito@users.noreply.github.com>
2022-07-24 23:04:40 -0400

    Versão final do complicador

--- src/main/java/com/marcelo/compilador/lexico/Main.java
+++ src/main/java/com/marcelo/compilador/lexico/Main.java
@@ -1,7 +1,6 @@
 package com.marcelo.compilador.lexico;
 
 import java.util.Scanner;
-import java.util.function.IntSupplier;
 import javax.script.*;
 
 public class Main {
@@ -18,60 +17,46 @@ public class Main {
 		Sintatico sintatico = new Sintatico();
 		calcular();
 		
-	
-		
 	}
-
 	public static void calcular() throws Exception{
-		
-		int cont = 0;
-		String expressao = "";
-		String aux = "";
+		StringBuilder expressao = new StringBuilder("");
+		int i = 0;
+		int j = 0;
+		int aux = 0;
+		String auxCalc = "";
+		while (i < TabelaDeSimbolos.VALOR.size()) {
+			if (!TabelaDeSimbolos.VALOR.get(i + 1).equals("^")){
+				expressao.append(TabelaDeSimbolos.VALOR.get(i));
+				i++;
+			}else{
+				String antes = TabelaDeSimbolos.VALOR.get(i);
+				String depois = TabelaDeSimbolos.VALOR.get(i + 2);
+				if (antes.equals(")")){
 
-		for(int i = 0; i < TabelaDeSimbolos.VALOR.size(); i++){
-
-			System.out.println(TabelaDeSimbolos.VALOR.get(i));
-
-			if(i == TabelaDeSimbolos.VALOR.size() - 1){
-				continue;
+					aux = i - 1;
+					auxCalc = "(";
+					while (!TabelaDeSimbolos.VALOR.get(aux).equals("("))
+					{
+						auxCalc += TabelaDeSimbolos.VALOR.get(aux);
+						expressao.deleteCharAt(aux);
+						aux = aux - 1;
+					}
+					expressao.deleteCharAt(aux);
+					auxCalc += ")";
+					expressao.append("Math.pow(" + auxCalc + "," + depois + ")");
+					//System.out.println(auxCalc);
+					i = i + 3;
+				}else{
+					expressao.append("Math.pow(" + antes + "," + depois + ")");
+					i = i + 3;
+				}
 			}
-			if(i == 0 && !TabelaDeSimbolos.VALOR.get(i + 1).equals("^")){
-				expressao += TabelaDeSimbolos.VALOR.get(i);
-				continue;
-			}
-			
-			if(TabelaDeSimbolos.VALOR.get(i + 1).equals("^")){
-				continue;
-			}
-
-			if(TabelaDeSimbolos.VALOR.get(i - 1).equals("^")){
-				continue;
-			}
-
-			if(TabelaDeSimbolos.VALOR.get(i).equals("^")){
-				String antes = TabelaDeSimbolos.VALOR.get(i - 1);
-				String depois = TabelaDeSimbolos.VALOR.get(i + 1);
-				//expressao += " Math.pow("+antes+","+depois+")";
-				aux = "Math.pow("+antes+","+depois+")";
-				expressao += aux;
-				continue;
-			}
-			System.out.println(expressao);
-			
 		}
-
-			
-		// create a script engine manager
+		System.out.println(expressao.toString());
 		ScriptEngineManager factory = new ScriptEngineManager();
-		// create a JavaScript engine
 		ScriptEngine engine = factory.getEngineByName("JavaScript");
-		// evaluate Ja/vaScript code from String
-		//Object obj = engine.eval(expressao);
-		Object test = engine.eval(aux);
-		//System.out.println (obj); 
-		//System.out.println (obj.getClass()); 
-		System.out.println(test);
-		
+		Object obj = engine.eval(expressao.toString());
+		System.out.println (obj);
 	}
 	
 }
